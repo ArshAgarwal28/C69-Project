64a84c79520993afc6e644f3c480902d617ebb56
ArshAgarwal28 <61740528+ArshAgarwal28@users.noreply.github.com>
2020-09-30 11:12:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
-
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 import * as Permissions from 'expo-permissions';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default class App extends React.Component {
+export default class TransactionScreen extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -34,28 +33,20 @@ export default class App extends React.Component {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
-
   render() {
     const { hasCameraPermission, scanned, buttonState } = this.state;
 
     if (buttonState === "clicked" && hasCameraPermission) {
       return (
         <BarCodeScanner
-          onBarCodeScanned={
-            scanned ? undefined : this.handleBarCodeScanned
-          }
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
         />
       );
     } else if (buttonState === "normal") {
       return (
         <View>
-          <Text>
-            {hasCameraPermission === true
-              ? this.state.scannedData
-              : "requestCameraPermissions"}
-          </Text>
-
-          <TouchableOpacity onPress={this.getPermissions}>
+          <TouchableOpacity style={styles.permissionButton} onPress={this.getPermissions}>
             <Text> Camera Permissions </Text>
           </TouchableOpacity>
         </View>
@@ -63,3 +54,13 @@ export default class App extends React.Component {
     }
   }
 }
+
+const styles = StyleSheet.create({
+  permissionButton: {
+    alignItems: 'center',
+    alignSelf: "center",
+    backgroundColor: 'red',
+    borderWidth: 2,
+    marginTop: 75
+  }
+})
